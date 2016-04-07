8ee6d6c7ddb7fa29ee086ac8c52260c99b50430e
Stefan Aeschbacher <stefan@aeschbacher.ch>
2016-04-07 22:41:25 +0200

    refactored handler_test.go

--- handler_test.go
+++ handler_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"io"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
@@ -11,16 +12,30 @@ import (
 	"testing"
 )
 
-func TestRespond(t *testing.T) {
+// creates a new recorder and a request
+func setupResponseRequest(
+	t *testing.T, method,
+	url string, data io.Reader) (*httptest.ResponseRecorder, *http.Request) {
+
 	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "http://example.com/foo", nil)
+	r, err := http.NewRequest(method, url, data)
 	if err != nil {
 		t.Fatal("Could not create request")
 	}
-	respond(w, r, http.StatusNotFound, "a message")
-	if w.Code != http.StatusNotFound {
-		t.Error("Status not set")
+	return w, r
+}
+
+// check a ResponseRecorder for a satus and reports Error(msg) if not correct
+func checkCode(t *testing.T, w *httptest.ResponseRecorder, code int, msg string) {
+	if w.Code != code {
+		t.Error(msg)
 	}
+}
+
+func TestRespond(t *testing.T) {
+	w, r := setupResponseRequest(t, "GET", "http://example.com/foo", nil)
+	respond(w, r, http.StatusNotFound, "a message")
+	checkCode(t, w, http.StatusNotFound, "Status not set")
 	if !strings.Contains(w.Body.String(), "404") {
 		t.Error("Body not set status")
 	}
@@ -36,16 +51,10 @@ func TestHandlePut(t *testing.T) {
 	res := createResource(db, resPath, true)
 
 	// put to item
-	w := httptest.NewRecorder()
 	data := strings.NewReader("some data")
-	r, err := http.NewRequest("PUT", "http://localhost:8080/asdf/qwer/path/res", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "PUT", "http://localhost:8080/asdf/qwer/path/res", data)
 	handlePut(db, w, r)
-	if w.Code != http.StatusOK {
-		t.Error("Put: 200 not working")
-	}
+	checkCode(t, w, http.StatusOK, "Put: 200 not working")
 	if strings.Compare(string(res.Value), "some data") != 0 {
 		t.Error("Put: Value not working")
 	}
@@ -53,28 +62,16 @@ func TestHandlePut(t *testing.T) {
 	resPath = []string{"a", "collection"}
 	res = createResource(db, resPath, false)
 
-	w = httptest.NewRecorder()
 	data = strings.NewReader("some data")
-	r, err = http.NewRequest("PUT", "http://localhost:8080/asdf/qwer/a/collection", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "PUT", "http://localhost:8080/asdf/qwer/a/collection", data)
 	handlePut(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Put: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Put: 404 not working")
 
 	// put to non-existens resource
-	w = httptest.NewRecorder()
 	data = strings.NewReader("some data")
-	r, err = http.NewRequest("PUT", "http://localhost:8080/asdf/qwer/new/item", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "PUT", "http://localhost:8080/asdf/qwer/new/item", data)
 	handlePut(db, w, r)
-	if w.Code != http.StatusCreated {
-		t.Error("Put: 201 not working")
-	}
+	checkCode(t, w, http.StatusCreated, "Put: 201 not working")
 }
 
 func TestHandlePost(t *testing.T) {
@@ -84,16 +81,10 @@ func TestHandlePost(t *testing.T) {
 	res := createResource(db, resPath, false)
 
 	// post to existing collection
-	w := httptest.NewRecorder()
 	data := strings.NewReader("some data")
-	r, err := http.NewRequest("POST", "http://localhost:8080/asdf/qwer/path/res", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "POST", "http://localhost:8080/asdf/qwer/path/res", data)
 	handlePost(db, w, r)
-	if w.Code != http.StatusCreated {
-		t.Error("Post: 201 not working")
-	}
+	checkCode(t, w, http.StatusCreated, "Post: 201 not working")
 	location := w.Header().Get("Location")
 	if strings.Compare("http://localhost:8080/asdf/qwer/path/res/0", location) != 0 {
 		t.Error("Post: Location wrong")
@@ -102,29 +93,17 @@ func TestHandlePost(t *testing.T) {
 		t.Error("Post: Value wrong")
 	}
 	// post to inexisting resource
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("POST", "http://localhost:8080/asdf/qwer/uwld/ere/i", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "POST", "http://localhost:8080/asdf/qwer/uwld/ere/i", data)
 	handlePost(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Post: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Post: 404 not working")
 
 	// post to item
 	resPath = []string{"an", "item"}
 	res = createResource(db, resPath, true)
 
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("POST", "http://localhost:8080/asdf/qwer/an/item", data)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "POST", "http://localhost:8080/asdf/qwer/an/item", data)
 	handlePost(db, w, r)
-	if w.Code != http.StatusConflict {
-		t.Error("Post: 409 not working")
-	}
+	checkCode(t, w, http.StatusConflict, "Post: 409 not working")
 }
 
 func TestHandlePostCommands(t *testing.T) {
@@ -142,32 +121,22 @@ func TestHandlePostCommands(t *testing.T) {
 	defer ts.Close()
 
 	// test hook created
-	w := httptest.NewRecorder()
 	hookData := strings.NewReader(fmt.Sprintf(`{"name": "hook_name", "url": "%s"}`, ts.URL))
-	r, err := http.NewRequest("POST", "http://localhost:8080/asdf/qwer/an_item/_hooks", hookData)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "POST", "http://localhost:8080/asdf/qwer/an_item/_hooks", hookData)
 	handlePost(db, w, r)
-	if w.Code != http.StatusCreated {
-		t.Error("Post Hook: 201 not working")
-	}
+	checkCode(t, w, http.StatusCreated, "Post Hook: 201 not working")
 	if !strings.Contains(res.Hooks.Hooks[0].Name, "hook_name") {
 		t.Error("Post Hook: Content not working")
 	}
 
 	// test hook called
-	w = httptest.NewRecorder()
 	postdata := strings.NewReader("any data")
-	r, err = http.NewRequest("POST", "http://localhost:8080/asdf/qwer/an_item", postdata)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "POST", "http://localhost:8080/asdf/qwer/an_item", postdata)
 	handlePost(db, w, r)
 	var data []byte
 	data = <-c
 	var hookevent HookEvent
-	err = json.Unmarshal(data, &hookevent)
+	err := json.Unmarshal(data, &hookevent)
 	if err != nil {
 		t.Error(err)
 	}
@@ -180,41 +149,23 @@ func TestHandleDelete(t *testing.T) {
 	createResource(db, resPath, true)
 
 	// delete intermediate resource
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/path", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/path", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Get: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Get: 404 not working")
 
 	// delete existing resource
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/path/res", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/path/res", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusOK {
-		t.Error("Delete: 200 not working")
-	}
+	checkCode(t, w, http.StatusOK, "Delete: 200 not working")
 	deletedRes, _ := getResource(db, resPath)
 	if strings.Compare(deletedRes.Name, "path") != 0 {
 		t.Error("Delete not working")
 	}
 
 	// delete a not-existing resource
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/asdfas/res", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/asdfas/res", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Get: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Get: 404 not working")
 }
 
 func TestHandleDeleteCommands(t *testing.T) {
@@ -226,37 +177,19 @@ func TestHandleDeleteCommands(t *testing.T) {
 	addHook(res, hookData)
 
 	// delete unknown hook
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/1", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/1", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Delete: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Get: 404 not working")
 
 	// delete weird path
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/0/gugu", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/0/gugu", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Delete: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Get: 404 not working")
 
 	// now delete the hook
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/0", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "DELETE", "http://localhost:8080/asdf/qwer/path/res/_hooks/0", nil)
 	handleDelete(db, w, r)
-	if w.Code != http.StatusOK {
-		t.Error("Delete: 200 not working")
-	}
+	checkCode(t, w, http.StatusOK, "Delete: 200 not working")
 	if len(res.Hooks.Hooks) > 0 {
 		t.Error("Delete Hook not working")
 	}
@@ -268,30 +201,18 @@ func TestHandleGet(t *testing.T) {
 	res := createResource(db, []string{"path", "res"}, true)
 
 	// request an existing resource
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "http://localhost:8080/asdf/qwer/path/res", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "GET", "http://localhost:8080/asdf/qwer/path/res", nil)
 	setValue(res, []byte("blup"))
 	handleGet(db, w, r)
-	if w.Code != http.StatusOK {
-		t.Error("Get: 200 not working")
-	}
+	checkCode(t, w, http.StatusOK, "Get: 200 not working")
 	if !strings.Contains(w.Body.String(), "blup") {
 		t.Error("Get: content not set")
 	}
 
 	// request a not-existing resource
-	w = httptest.NewRecorder()
-	r, err = http.NewRequest("GET", "http://localhost:8080/asdf/qwer/asdfas/res", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r = setupResponseRequest(t, "GET", "http://localhost:8080/asdf/qwer/asdfas/res", nil)
 	handleGet(db, w, r)
-	if w.Code != http.StatusNotFound {
-		t.Error("Get: 404 not working")
-	}
+	checkCode(t, w, http.StatusNotFound, "Get: 404 not working")
 }
 
 func TestHandleGetHooks(t *testing.T) {
@@ -301,16 +222,9 @@ func TestHandleGetHooks(t *testing.T) {
 	hookData := []byte(fmt.Sprintf(`{"name": "a_hook", "url": "http://test.com/a/hook"}`))
 	addHook(res, hookData)
 
-	w := httptest.NewRecorder()
-	r, err := http.NewRequest("GET", "http://localhost:8080/asdf/qwer/path/res/_hooks", nil)
-	if err != nil {
-		t.Fatal("Could not create request")
-	}
+	w, r := setupResponseRequest(t, "GET", "http://localhost:8080/asdf/qwer/path/res/_hooks", nil)
 	handleGet(db, w, r)
-
-	if w.Code != http.StatusOK {
-		t.Error("Get hooks: 200 not working")
-	}
+	checkCode(t, w, http.StatusOK, "Get hooks: 200 not working")
 	if !strings.Contains(w.Body.String(), "test.com") {
 		t.Error("Get: content not set")
 	}
